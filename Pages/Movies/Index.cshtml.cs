6988d0b3bc108c27b59ec935ab16647db402f7a9
Wojtyla-spec <wojkom777@protonmail.com>
2024-06-02 14:20:31 +0200

    Razor filtering movies by genre and tiltle

---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace stronaZRazorem.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public IndexModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get; set; } = default!;
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var genreQuery = _context.Movie.OrderBy(m => m.Genre).Select(m => m.Genre);
+            var movies = _context.Movie.AsQueryable();
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => EF.Functions.Like(s.Title.ToLower(), $"%{SearchString.ToLower()}%"));
+            }
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
+        }
+    }
+}
+
